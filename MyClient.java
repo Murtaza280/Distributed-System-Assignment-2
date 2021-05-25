d9f45354a43ed1c15bd0e5efabc6f3165687a9db
Murtaza280 <48035581+Murtaza280@users.noreply.github.com>
2021-05-25 22:50:18 +1000

    updaetd myclient 5.2

--- MyClient.java
+++ MyClient.java
@@ -4,12 +4,17 @@ import java.nio.charset.StandardCharsets;
 public class MyClient {
 
 
+		static DataInputStream din;
+		static PrintStream dout;
+		static Socket s;
+
 	static String response;
 	static int nRec = 0, nLen = 0;
 
 	static String largest=" ";
 
 	public static void main(String[] args) throws Exception {
+		
 		s = new Socket("localhost", 50000);
 		din = new DataInputStream(s.getInputStream());
 		dout = new PrintStream(s.getOutputStream(), true);
