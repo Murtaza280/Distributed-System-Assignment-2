5edfd624e69b623daff82a59c777d5b9c575d154
Murtaza280 <48035581+Murtaza280@users.noreply.github.com>
2021-05-25 22:51:26 +1000

    updated myclient 5.3

--- MyClient.java
+++ MyClient.java
@@ -1,12 +1,12 @@
 import java.io.*;
 import java.net.*;
 import java.nio.charset.StandardCharsets;
+
 public class MyClient {
 
-
-		static DataInputStream din;
-		static PrintStream dout;
-		static Socket s;
+	static DataInputStream din;
+	static PrintStream dout;
+	static Socket s;
 
 	static String response;
 	static int nRec = 0, nLen = 0;
@@ -14,14 +14,14 @@ public class MyClient {
 	static String largest=" ";
 
 	public static void main(String[] args) throws Exception {
-		
+
 		s = new Socket("localhost", 50000);
 		din = new DataInputStream(s.getInputStream());
 		dout = new PrintStream(s.getOutputStream(), true);
+
 		send("HELO");
 		send("AUTH " + System.getProperty("user.name"));
 		send("REDY");
-
 		send("QUIT");
 	}
 }
